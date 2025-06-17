280f0d8408474528aad9aa51d001caa23480f9cb
poria neyshaboriyan <poria.nsh@gmail.com>
2025-06-17 21:54:11 +0330

    test

--- frontend/start/src/app/blogs/(PostList)/layout.jsx
+++ frontend/start/src/app/blogs/(PostList)/layout.jsx
@@ -1,18 +1,19 @@
-
-import { Suspense } from 'react';
-import { Spinner } from '@/ui/Spinner';
-import CategoryList from '@/components/blog/CategoryList';
+import {Suspense} from "react";
+import {Spinner} from "@/ui/Spinner";
+import CategoryList from "@/components/blog/CategoryList";
 
 function Bloglayout({children}) {
   return (
-    <div>
+    <div className="fgfvf">
       <h1>لیست بلاگ ها</h1>
       <div className="grid grid-cols-12 gap-8 mt-5">
-        <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">{
-          <Suspense fallback={<Spinner/>}>
-            <CategoryList />
-          </Suspense>
-        }</div>
+        <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
+          {
+            <Suspense fallback={<Spinner />}>
+              <CategoryList />
+            </Suspense>
+          }
+        </div>
         <div className="col-span-12 lg:col-span-8 xl:col-span-9">{children}</div>
       </div>
     </div>
