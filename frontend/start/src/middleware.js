3abc955bdbc676e5c29ebf22f01904417ef052eb
poria neyshaboriyan <poria.nsh@gmail.com>
2025-06-24 23:05:21 +0330

    test

---
+++ frontend/start/src/middleware.js
@@ -0,0 +1,22 @@
+import {NextResponse} from "next/server";
+import middlewareAuth from "./utils/middlewareAuth";
+
+export async function middleware(req) {
+  const url = req.url;
+  const pathname = req.nextUrl.pathname;
+
+  if (pathname.startsWith("/profile")) {
+    const user = await middlewareAuth(req);
+    if (!user) return NextResponse.redirect(new URL(`/auth`, req.nextUrl));
+  }
+  if (pathname.startsWith("/auth")) {
+    const user = await middlewareAuth(req);
+    if (user) return NextResponse.redirect(new URL(`/`, req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/profile/:path*","/auth"],
+};
