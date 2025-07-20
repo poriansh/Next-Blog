4e35b8751e7a0898c8bf9c8f29292039f7aa1a2c
Pouria Neishabourian <pouria.neyshabourian@swai.ai>
2025-07-20 15:12:32 +0330

    chore: update dependencies and improve code formatting
    - Updated nodemon to version 3.1.10 in backend/package.json
    - Updated cookie to version 1.0.2 in backend/package.json
    - Updated jsonwebtoken to version 9.0.2 in backend/package.json
    - Updated various ESLint packages in frontend/start/package-lock.json for better performance and compatibility
    - Refactored layout.jsx for improved readability and consistency in formatting

--- frontend/start/src/app/blogs/(PostList)/layout.jsx
+++ frontend/start/src/app/blogs/(PostList)/layout.jsx
@@ -1,23 +1,23 @@
-import {Suspense} from "react";
-import {Spinner} from "@/ui/Spinner";
+import { Suspense } from "react";
+import { Spinner } from "@/ui/Spinner";
 import CategoryList from "@/components/blog/CategoryList";
 import Header from "@/components/Header";
 
-function Bloglayout({children}) {
+function Bloglayout({ children }) {
   return (
     <>
-      <Header />  
+      <Header />
       <div className="container">
         <h1>لیست بلاگ ها</h1>
         <div className="grid grid-cols-12 gap-8 mt-5">
           <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-appsecondary-500 space-y-4">
-            {
-              <Suspense fallback={<Spinner />}>
-                <CategoryList />
-              </Suspense>
-            }
+            <Suspense fallback={<Spinner />}>
+              <CategoryList />
+            </Suspense>
           </div>
-          <div className="col-span-12 lg:col-span-8 xl:col-span-9">{children}</div>
+          <div className="col-span-12 lg:col-span-8 xl:col-span-9">
+            {children}
+          </div>
         </div>
       </div>
     </>
