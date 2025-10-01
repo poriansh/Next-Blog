fdd5b23aa3b76791e6eeede37fa9189283e78324
Pouria Neishabourian <pouria.neyshabourian@swai.ai>
2025-10-01 18:45:00 +0330

    fix: add comment to clarify dynamic rendering issue with axios in PostServices.js

--- frontend/start/src/services/PostServices.js
+++ frontend/start/src/services/PostServices.js
@@ -12,6 +12,7 @@ export async function getSinglePosts(slug) {
   }
 }
 
+// چون از axios استفاده کردی، Next.js نتونسته بفهمه ریکوئستت dynamic ـه → نتیجه: صفحه Static شده.
 export async function getPosts(option) {
   try {
     const { data } = await http.get(`/post/list?categorySlug=${option}`);
