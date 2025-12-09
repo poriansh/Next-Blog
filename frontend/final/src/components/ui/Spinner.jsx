4b8654d7387e6a042a9effe3e0cf317a7dcd31af
poria neyshaboriyan <poria.nsh@gmail.com>
2025-12-09 23:25:31 +0330

    feat: initialize frontend structure with essential configurations and components
    - Added .gitignore to exclude unnecessary files from version control.
    - Created eslint.config.js for linting configuration tailored for Next.js.
    - Introduced jsconfig.json for JavaScript project settings.
    - Set up next.config.mjs for Next.js configuration.
    - Established package.json and package-lock.json for dependency management.
    - Configured postcss for styling with Tailwind CSS.
    - Added README.md for project documentation and deployment instructions.
    - Implemented tailwind.config.mjs for Tailwind CSS customization.
    - Removed outdated final directory and its contents, streamlining the project structure.
    - Included initial font files and images for the frontend.

--- frontend/final/src/components/ui/Spinner.jsx
+++
@@ -1,7 +0,0 @@
-export function Spinner() {
-  return <div className="spinner"></div>;
-}
-
-export function SpinnerMini() {
-  return <div className="spinner-mini"></div>;
-}
--- frontend/final/src/hook/useMoveBack.js
+++
@@ -1,6 +0,0 @@
-import {useRouter} from "next/navigation";
-
-export default function useMoveBack() {
-  const router = useRouter();
-  return () => router.back();
-}
